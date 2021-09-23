0d35965c352a363f07703b348e0363ddfae3e0ab
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2021-09-23 18:24:49 -0400

    Created file; wrote CelsiusToFahrenheit function

---
+++ CtoFTester.java
@@ -0,0 +1,9 @@
+// Each function should take a double input and return a double output.
+// One function, has a parameter of fahr and the other has a parameter of cels
+
+
+public class CtoFTester{
+  public static void celsiusToFahrenheit(double cels) {
+    return (9/5)*(cels)+32
+  }
+}
